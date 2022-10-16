a0f096d00feb1589668970feef0770da58877596
henriquesd <hsd.foz@gmail.com>
2022-10-16 21:17:45 +0200

    For of Loop

---
+++ ForOfLoop/index.js
@@ -0,0 +1,20 @@
+let incomes = [62000, 67000, 75000];
+let total = 0;
+
+console.log(`Initial Total: ${total}`);
+
+for (const income of incomes) {
+    console.log(income);
+    total += income;
+}
+
+console.log(`Total: ${total}`);
+
+
+// ---------------------------
+
+let fullName = "Henrique Siebert Domareski";
+
+for (const char of fullName) {
+    console.log(char);
+}
