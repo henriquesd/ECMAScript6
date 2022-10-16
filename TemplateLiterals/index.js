44fcab0748b6464d8115aeaee83c65ee22fee7ac
henriquesd <hsd.foz@gmail.com>
2022-10-16 20:46:51 +0200

    Template Literals

---
+++ TemplateLiterals/index.js
@@ -0,0 +1,10 @@
+let word1 = 'Henrique';
+let word2 = 'Siebert Domareski';
+let num1 = 2;
+let num2 = 3;
+
+const lastNameWithSumNumber = `${num1 + num2} ${word2}`;
+console.log(lastNameWithSumNumber);
+
+let fullName = `${word1} ${word2}`;
+console.log(fullName)
