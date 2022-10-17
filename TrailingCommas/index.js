047ec6fbc612dbf636d7f1be53f934507a84c325
henriquesd <hsd.foz@gmail.com>
2022-10-17 21:35:43 +0200

    Trailing Commas

---
+++ TrailingCommas/index.js
@@ -0,0 +1,10 @@
+// Now there is no issues anymore when using trailing commas (in the past you would get an error);
+function add(param1,) {
+    const example = {
+        name: 'Henrique',
+    }
+
+    console.log(example);
+}
+
+add(2);
